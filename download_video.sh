17659bc18c1ff9c7d3280a165ac98f575566a638
Artur <git@artursterz.de>
2019-05-03 11:45:14 +0200

    Prepare LCN submission

---
+++ download_video.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+
+wget -r -l 1 -P ./video http://dash.akamaized.net/dash264/TestCasesHD/2c/qualcomm/1/
---
+++ experiment_params.js
@@ -0,0 +1,9 @@
+document.experimentParams = {
+    mpdUrl: "/dash/dash.akamaized.net/dash264/TestCasesHD/2c/qualcomm/1/MultiResMPEG2.mpd",
+    experimentID: "1",
+    bwEstimationMethod: "bola",
+    bwEstimationHalflife: "None",
+    plyerType: "DASH.JS",
+    bufferlevel: 10,
+    playTime: 0
+};
